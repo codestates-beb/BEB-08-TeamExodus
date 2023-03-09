05b68eb91b1869cbf1296b4433ac88f91a356a6d
JayTheMiner <jaytheminer7@gmail.com>
2023-03-10 01:20:56 +0900

    NFT 발행 테스트

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,4 +1,8 @@
 import styled from "styled-components";
+import Web3 from "web3";
+import erc721abi from "../erc721abi";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Container = styled.div`
   padding: 50px 200px;
@@ -59,35 +63,140 @@ const Button = styled.div`
 `;
 
 function Create() {
+    const [newErc721addr, setNewErc721Addr] = useState();
+    const [newTitle, setTitle] = useState();
+    const [newArtist, setArtist] = useState();
+    const [newDesc, setDesc] = useState();
+    const [web3, setWeb3] = useState();
+    const [account, setAccount] = useState("");
+    // 컨트랙트 주소 하드코딩 - 테스트용으로 수정 필요
+    const contractAddr = "0x66BA8423CA77AaDE79ce269FB14A52DeD6532b83"
+    
+    const addNewErc721Token = async () => {
+        // const tokenContract = await new web3.eth.Contract(
+        //     erc721abi,
+        //     newErc721addr
+        // );
+        const tokenContract = await new web3.eth.Contract(
+            erc721abi,
+            contractAddr
+        );
+        // const name = await tokenContract.methods.name().call();
+        // const symbol = await tokenContract.methods.symbol().call();
+        const TotalSupply = await tokenContract.methods.TotalSupply().call();
+        // const ownerOf = await tokenContract.methods.ownerOf(1).call();
+        try{
+
+            const tokenURI1 = await tokenContract.methods.tokenURI(1).call();
+            const tokenURI2 = await tokenContract.methods.tokenURI(2).call();
+            // const tokenURI3 = await tokenContract.methods.tokenURI(0).call();
+            console.log(tokenURI1, tokenURI2)
+
+        } catch(e){
+            console.log(e)
+        }
+
+
+        // const currentAddr = localStorage.getItem("isLoggedIn")
+        const currentAddr = "0x520186f00a0409a8aeb47337989727cefe69bbc2" // 김진호 메타마스크 지갑
+        const jsonAddr = "https://files.mybox.naver.com/.fileLink/MA5tGzPkA6f9R%2F0vvuqT7VtrovZVRZsAInCD%2F%2FxVDxo5YlEdryIle30gHfP4kvU702dF5qDoU8fVyNv%2FmwMmJgQ%3D/subinchoi.json?authtoken=m8xYnEv4d5LXFobzV%2FUhzQI%3D"
+        console.log(setTitle)
+
+        try{
+            const mintNFT = await tokenContract.methods.mintNFT(
+                currentAddr,
+                jsonAddr,
+                newTitle,
+                newArtist,
+                newDesc).send({from:currentAddr, gas: 0xf4240})
+            
+            console.log(mintNFT)
+            
+        }
+     
+        catch (e) {
+            console.log(e)
+        }
+
+
+        // console.log("tokenContract: ", tokenContract);
+        // console.log("ownerOf: ", ownerOf);
+        // console.log("name: ", name);
+        // console.log("symbol: ", symbol);
+        // console.log("tokenURI: ", tokenURI);
+        console.log("totalSupply: ", TotalSupply);
+        
+    };
+
+    useEffect(() => {
+        if (typeof window.ethereum !== "undefined") {
+            // window.ethereum이 있다면
+            try {
+                const web = new Web3(window.ethereum); // 새로운 web3 객체를 만든다
+                setWeb3(web);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    }, []);
+    
+
+
+
   return (
     <Container>
       <FormBox>
         <Title>Create New NFT</Title>
         <InputBox>
           <Label>Image, Video, Audio or 3D Model **** </Label>
-          <Input placeholder="Find Directory..." />
+          <Input
+            type="text"
+            placeholder="Find Directory..."/>
         </InputBox>
         <InputBox>
-          <Label>Name * </Label>
-          <Input placeholder="Item Name" />
+          <Label>Title</Label>
+          <Input 
+          type="text"
+          placeholder="Item Name" 
+          onChange={(e) => {
+            setTitle(e.target.value);
+        }} />
         </InputBox>
         <InputBox>
-          <Label>External Link</Label>
-          <Input placeholder="https://yoursite.io/item/123" />
+          <Label>Artist </Label>
+          <Input 
+          type="text"
+          placeholder="Item Name"
+          onChange={(e)=>{
+            setArtist(e.target.value);
+          }} />
         </InputBox>
         <InputBox>
           <Label>Description</Label>
-          <Input placeholder="Provide a detailed description of you item." />
+          <Input 
+            type="text"
+            placeholder="Provide a detailed description of you item."
+            onChange={(e)=>{
+                setDesc(e.target.value);
+            }} />
         </InputBox>
-        <InputBox>
+        {/* <InputBox>
+          <Label>Name * </Label>
+          <Input placeholder="Item Name" />
+        </InputBox> */}
+        {/* <InputBox>
+          <Label>External Link</Label>
+          <Input placeholder="https://yoursite.io/item/123" />
+        </InputBox> */}
+        {/* <InputBox>
           <Label>Collection</Label>
           <Input placeholder="Select Collection" />
-        </InputBox>
-        <InputBox>
+        </InputBox> */}
+        {/* <InputBox>
           <Label>Blockchain</Label>
           <Input placeholder="here" />
-        </InputBox>
-        <Button>Create</Button>
+        </InputBox> */}
+        <Button onClick={addNewErc721Token}>Create</Button>
       </FormBox>
     </Container>
   );
