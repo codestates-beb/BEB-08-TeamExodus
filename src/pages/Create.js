04aff899d68888ebeffdfe3c652d84687cc29e52
DreamBoysYJ <89343745+DreamBoysYJ@users.noreply.github.com>
2023-03-10 22:55:07 +0900

    create page finished

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,10 +1,14 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import Web3 from "web3";
+import erc721abi from "../erc721abi";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
   padding: 50px 200px;
   margin-top: 50px;
 
-  background-color: pink;
   display: flex;
   font-size: 25px;
 
@@ -12,9 +16,13 @@ const Container = styled.div`
   align-items: center;
 `;
 
-const FormBox = styled.div`
+const FormBox = styled.form`
   width: 600px;
   height: 100%;
+  background-color: beige;
+  padding: 15px 15px;
+  border: 3px solid black;
+  border-radius: 20px;
 `;
 
 const Title = styled.div`
@@ -46,7 +54,21 @@ const Input = styled.input`
   }
 `;
 
-const Button = styled.div`
+const MintBtn = styled(Input)`
+  background-color: #2081e2;
+  font-weight: 600;
+  :hover {
+    cursor: pointer;
+    scale: 1.15;
+  }
+`;
+
+const AlertBox = styled.div`
+  width: 1000px;
+  height: 1000px;
+`;
+
+/* const AlertBox = styled.input`
   width: 100px;
   height: 50px;
   font-size: 18px;
@@ -56,39 +78,99 @@ const Button = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+`; */
+
+const AlertBtn = styled.div`
+  background-color: aqua;
 `;
 
 function Create() {
+  const [web3, setWeb3] = useState();
+  const [minted, setMinted] = useState(true);
+
+  let contractAddr = "0xc08F3536a11A72bcD25CbBc25192C4981C4E3E65";
+  const userAddr = localStorage.getItem("isLoggedIn");
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    const { title, artist, description, image_uri } = data;
+    const tokenContract = await new web3.eth.Contract(erc721abi, contractAddr);
+
+    const mint = await tokenContract.methods
+      .mintNFT(userAddr, image_uri, title, artist, description)
+      .send({ from: userAddr })
+      .on("receipt", console.log("MINIT!!!"));
+    const totalSupply = await tokenContract.methods.TotalSupply().call();
+    console.log("total", totalSupply);
+    console.log(mint);
+    setMinted(true);
+  };
+
+  useEffect(() => {
+    setMinted(false);
+    if (typeof window.ethereum !== "undefined") {
+      // window.ethereum이 있다면
+      try {
+        const web = new Web3(window.ethereum); // 새로운 web3 객체를 만든다
+        setWeb3(web);
+        console.log("set web");
+        console.log(web);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }, []);
+
   return (
     <Container>
-      <FormBox>
-        <Title>Create New NFT</Title>
-        <InputBox>
-          <Label>Image, Video, Audio or 3D Model **** </Label>
-          <Input placeholder="Find Directory..." />
-        </InputBox>
-        <InputBox>
-          <Label>Name * </Label>
-          <Input placeholder="Item Name" />
-        </InputBox>
-        <InputBox>
-          <Label>External Link</Label>
-          <Input placeholder="https://yoursite.io/item/123" />
-        </InputBox>
-        <InputBox>
-          <Label>Description</Label>
-          <Input placeholder="Provide a detailed description of you item." />
-        </InputBox>
-        <InputBox>
-          <Label>Collection</Label>
-          <Input placeholder="Select Collection" />
-        </InputBox>
-        <InputBox>
-          <Label>Blockchain</Label>
-          <Input placeholder="here" />
-        </InputBox>
-        <Button>Create</Button>
-      </FormBox>
+      {minted ? (
+        <div>씨발</div>
+      ) : (
+        /* <AlertBox>
+          <span>Minting Complete</span>
+          <AlertBtn>
+            <Link to="/users/1">OK</Link>
+          </AlertBtn>
+        </AlertBox> */
+        <FormBox onSubmit={handleSubmit(onSubmit)}>
+          <Title>Create New NFT</Title>
+          <InputBox>
+            <Label> Title </Label>
+            <Input
+              placeholder="Title of your nft..."
+              {...register("title", { required: true })}
+            />
+          </InputBox>
+          <InputBox>
+            <Label> Artist Name </Label>
+            <Input
+              placeholder="Creater of your nft..."
+              {...register("artist", { required: true })}
+            />
+          </InputBox>
+          <InputBox>
+            <Label> Description </Label>
+            <Input
+              placeholder="Provide a detailed description of you item."
+              {...register("description", { required: true })}
+            />
+          </InputBox>
+          <InputBox>
+            <Label>Image URI</Label>
+            <Input
+              placeholder="Copy your NFT Image URI..."
+              {...register("image_uri", { required: true })}
+            />
+          </InputBox>
+
+          <MintBtn type="submit" value={"Mint"} />
+        </FormBox>
+      )}
     </Container>
   );
 }
